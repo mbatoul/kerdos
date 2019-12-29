072a276d0a2e14903bde028d775fc5796bbd05a0
MathisFlatlooker <mathis@flatlooker.com>
2019-12-29 22:02:43 +0100

    Fix app launching

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 import os
 from flask import Flask, render_template
-# from flask_sqlalchemy import SQLAlchemy
 
 def create_app(test_config=None):
   # create and configure the app
+
   app = Flask(__name__, instance_relative_config=True)
+
   app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
   )
+
+  if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
   if test_config is None:
     # load the instance config, if it exists, when not testing
@@ -16,7 +22,6 @@ def create_app(test_config=None):
   else:
     # load the test config if passed in
     app.config.from_mapping(test_config)
-
 
   # ensure the instance folder exists
   try:
@@ -36,3 +41,4 @@ def create_app(test_config=None):
 
   return app
 
+app = create_app()
