69c1d59649de38be8e0a4bc11646be54b0e97ca1
mbatoul <mathis.batoul@hotmail.fr>
2019-12-30 00:41:40 +0100

    Clean the procfile. Create /trading folder for scripts.

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,7 @@ import os
 from flask import Flask, render_template
 
 def create_app(test_config=None):
-  # create and configure the app
-
+  # App initialization
   app = Flask(__name__, instance_relative_config=True)
 
   app.config.from_mapping(
@@ -12,27 +11,27 @@ def create_app(test_config=None):
   )
 
   if test_config is None:
-    # load the instance config, if it exists, when not testing
     app.config.from_pyfile('config.py', silent=True)
   else:
-    # load the test config if passed in
     app.config.from_mapping(test_config)
 
-  # ensure the instance folder exists
   try:
     os.makedirs(app.instance_path)
   except OSError:
     pass
 
-  @app.route('/')
-  def home():
-    return render_template('home.html')
-
+  # Database initialization
   from . import db
   db.init_app(app)
 
+  # Authentication blueprint
   from . import auth
   app.register_blueprint(auth.auth)
+
+  # Main route
+  @app.route('/')
+  def home():
+    return render_template('home.html')
 
   return app
 
